43c056ddfd1f5f789cff482fe53bfd27639058c2
devsuzarte <ben.dantassuzarte@gmail.com>
2025-01-24 11:49:56 -0300

    feat: add navbar links and home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TimeComponent } from './pages/time/time.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    { path: "time", component: TimeComponent }
+    { path: "timers", component: TimeComponent },
+    { path: "", component: HomeComponent }
 ];
